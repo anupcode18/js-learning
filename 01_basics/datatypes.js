32c0ff5efe0825603143957da70ee12cf755f4c7
anupcode18 <anupagrawal895@gmail.com>
2026-06-06 15:14:53 +0000

    datatype learning & variable

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+// alert ( 3 + 3 ) // we are using nodejs, not browser
+
+console.log("anup");
+console.log(3 + 3);  // syntax of the code should be readable 
+
+
+/*
+datatypes :
+1. number -> 2 to power 53
+2. bigint
+3. string -> "words/variables"
+4 boolean -> true/false
+5. null standalone value 
+6. undefined -> variable is decalred but value is not given
+ex -> let name;
+7. symbol -> imp for react framework
+
+*/
+
+let name = "anup2"
+let age = 20;
+let ageCorrect = true; // boolean datatype
+let state;  // undefined
+
+console.log(typeof "anup2") // to check datatype of value
+
+console.log(typeof age); // to ceck to dt of varialbe
---
+++ 01_basics/variables.js
@@ -0,0 +1,20 @@
+const accountId = 12345
+let accountEmail = "anup123@gmail.com"
+var accountPassword = 1212
+accountCity = "pune"
+
+// accountId = 2 // not allowed
+
+accountEmail = "anup@gmail.com"
+accountPassword = 223344
+accountCity = "bengluru"
+let accountState;
+
+console.log(accountState);
+
+/*
+prefer not to use var
+because of issue in block scope  and functional scope
+*/
+
+// console.table([accountId, accountEmail, accountPassword, accountCity])
